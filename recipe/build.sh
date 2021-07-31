19993df9fb2bd504fa4757eea0772d3ea22c493b
lorenz <looooo@users.noreply.github.com>
2021-07-31 14:35:48 +0200

    Remove dir lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ fi
 declare -a CMAKE_PLATFORM_FLAGS
 
 if [[ ${HOST} =~ .*linux.* ]]; then
+  rm -rf ${PREFIX}/lib64
   echo "adding hacks for linux"
   # temporary workaround for vtk-cmake setup
   # should be applied @vtk-feedstock
