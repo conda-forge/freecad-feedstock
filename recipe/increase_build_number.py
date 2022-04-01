33f99712bf713513035eda9fb3a97c7fc2aae75b
looooo <sppedflyer@gmail.com>
2022-04-01 18:54:58 +0200

    wrong path

--- recipe/increase_build_number.py
+++ recipe/increase_build_number.py
@@ -2,12 +2,12 @@
 
 import re
 
-with open("meta.yaml", "r") as f:
+with open("recipe/meta.yaml", "r") as f:
     text = f.read()
 
 build_number_old = int(re.search(r"(?<={% set build_number = )\d+", text)[0])
 build_number_new = build_number_old + 1
 print(f"set build_number from build_number {build_number_old} to {build_number_new}")
 text = re.sub(r"(?<={% set build_number = )\d+", str(build_number_new), text)
-with open("meta.yaml", "w") as f:
+with open("recipe/meta.yaml", "w") as f:
     f.write(text)
