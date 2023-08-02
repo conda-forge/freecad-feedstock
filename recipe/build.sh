9b866d5f5b00cc8c63349654c135dc5fa06eab73
Adrián Insaurralde Avalos <adrianinsaval@gmail.com>
2023-08-01 23:38:05 -0400

    update to 0.21.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,11 +11,7 @@ declare -a CMAKE_PLATFORM_FLAGS
 
 if [[ ${HOST} =~ .*linux.* ]]; then
   echo "adding hacks for linux"
-  # temporary workaround for vtk-cmake setup
-  # should be applied @vtk-feedstock
-  # sed -i '380,384d' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets.cmake
 
-  # sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g' ${PREFIX}/lib/cmake/vtk-8.2/Modules/vtkhdf5.cmake 
   # temporary workaround for qt-cmake:
   sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
   sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
@@ -26,43 +22,43 @@ fi
 if [[ ${HOST} =~ .*darwin.* ]]; then
   # add hacks for osx here!
   echo "adding hacks for osx"
-
-
-  # should be applied @vtk-feedstock
-  # sed -i '381,383d' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets.cmake
-
-  ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
+  
+  ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
   ln -s /Applications/Xcode.app /Applications/Xcode_11.7.app
 
   # install space-mouse
-  curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-6-6_360DF97D-ED08-4ccf-A55E-0BF905E58476/3DxWareMac_v10-6-6_r3234.dmg'
+  if [[ ${target_platform} =~ osx-64 ]]; then
+    /usr/bin/curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-6-6_360DF97D-ED08-4ccf-A55E-0BF905E58476/3DxWareMac_v10-6-6_r3234.dmg'
+  else
+    /usr/bin/curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
+  fi
   hdiutil attach -readonly /tmp/3dFW.dmg
   sudo installer -package /Volumes/3Dconnexion\ Software/Install\ 3Dconnexion\ software.pkg -target /
   diskutil eject /Volumes/3Dconnexion\ Software
   CMAKE_PLATFORM_FLAGS+=(-DFREECAD_USE_3DCONNEXION:BOOL=ON)
   CMAKE_PLATFORM_FLAGS+=(-D3DCONNEXIONCLIENT_FRAMEWORK:FILEPATH="/Library/Frameworks/3DconnexionClient.framework")
+
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
-
 cmake -G "Ninja" \
-      -D BUID_WITH_CONDA:BOOL=ON \
+      -D BUILD_WITH_CONDA:BOOL=ON \
       -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH="$PREFIX" \
       -D CMAKE_PREFIX_PATH:FILEPATH="$PREFIX" \
       -D CMAKE_LIBRARY_PATH:FILEPATH="$PREFIX/lib" \
       -D CMAKE_INSTALL_LIBDIR:FILEPATH="$PREFIX/lib" \
       -D CMAKE_INCLUDE_PATH:FILEPATH="$PREFIX/include" \
-      -D BUILD_QT5:BOOL=ON \
       -D FREECAD_USE_OCC_VARIANT="Official Version" \
       -D OCC_INCLUDE_DIR:FILEPATH="$PREFIX/include" \
       -D USE_BOOST_PYTHON:BOOL=OFF \
       -D FREECAD_USE_PYBIND11:BOOL=ON \
       -D SMESH_INCLUDE_DIR:FILEPATH="$PREFIX/include/smesh" \
       -D FREECAD_USE_EXTERNAL_SMESH=ON \
+      -D FREECAD_USE_EXTERNAL_FMT:BOOL=OFF \
       -D BUILD_FLAT_MESH:BOOL=ON \
       -D BUILD_WITH_CONDA:BOOL=ON \
-      -D PYTHON_EXECUTABLE:FILEPATH="$PYTHON" \
-      -D Python3_EXECUTABLE="$PYTHON" \
+      -D Python_EXECUTABLE:FILEPATH="$PYTHON" \
+      -D Python3_EXECUTABLE:FILEPATH="$PYTHON" \
       -D BUILD_FEM_NETGEN:BOOL=ON \
       -D BUILD_SHIP:BOOL=OFF \
       -D OCCT_CMAKE_FALLBACK:BOOL=OFF \
