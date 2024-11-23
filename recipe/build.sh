9e94d5fcea1a0c045ce87d7ede7fc85cc0d422ae
Lorenz Lechner <sppedflyer@gmail.com>
2024-11-23 10:38:54 +0100

    copy files from freecad/freecad-feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,10 @@ cd build
 
 if [[ ${FEATURE_DEBUG} = 1 ]]; then
       BUILD_TYPE="Debug"
+      DEV_TESTS="ON"
 else
       BUILD_TYPE="Release"
+      DEV_TESTS="OFF"
 fi
 
 declare -a CMAKE_PLATFORM_FLAGS
@@ -23,15 +25,11 @@ if [[ ${HOST} =~ .*darwin.* ]]; then
   # add hacks for osx here!
   echo "adding hacks for osx"
   
-  ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
-  ln -s /Applications/Xcode.app /Applications/Xcode_11.7.app
+  # delete python3.11 from framework
+  # rm -rf /Library/Frameworks/Python.framework/Versions/3.11
 
   # install space-mouse
-  if [[ ${target_platform} =~ osx-64 ]]; then
-    /usr/bin/curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-6-6_360DF97D-ED08-4ccf-A55E-0BF905E58476/3DxWareMac_v10-6-6_r3234.dmg'
-  else
-    /usr/bin/curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
-  fi
+  /usr/bin/curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
   hdiutil attach -readonly /tmp/3dFW.dmg
   sudo installer -package /Volumes/3Dconnexion\ Software/Install\ 3Dconnexion\ software.pkg -target /
   diskutil eject /Volumes/3Dconnexion\ Software
@@ -40,7 +38,7 @@ if [[ ${HOST} =~ .*darwin.* ]]; then
 
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
-cmake ${CMAKE_ARGS} -G "Ninja" \
+cmake -G "Ninja" \
       -D BUILD_WITH_CONDA:BOOL=ON \
       -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH="$PREFIX" \
@@ -64,16 +62,13 @@ cmake ${CMAKE_ARGS} -G "Ninja" \
       -D OCCT_CMAKE_FALLBACK:BOOL=OFF \
       -D FREECAD_USE_QT_DIALOG:BOOL=ON \
       -D BUILD_DYNAMIC_LINK_PYTHON:BOOL=OFF \
-      -D Boost_NO_BOOST_CMAKE:BOOL=ON \
       -D FREECAD_USE_PCL:BOOL=ON \
       -D FREECAD_USE_PCH:BOOL=OFF \
       -D INSTALL_TO_SITEPACKAGES:BOOL=ON \
+      -D ENABLE_DEVELOPER_TESTS:BOOL="${DEV_TESTS}" \
       ${CMAKE_PLATFORM_FLAGS[@]} \
       ..
 
-echo "FREECAD_USE_3DCONNEXION=${FREECAD_USE_3DCONNEXION}"
-
 ninja install
-rm -r ${PREFIX}/share/doc/FreeCAD     # smaller size of package!
 mv ${PREFIX}/bin/FreeCAD ${PREFIX}/bin/freecad
 mv ${PREFIX}/bin/FreeCADCmd ${PREFIX}/bin/freecadcmd
